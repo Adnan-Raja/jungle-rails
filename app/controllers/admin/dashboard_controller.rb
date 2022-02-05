4b90ff9cb4d7c006fa224d6a6ee4fec800491674
Adnan-Raja <adnan.saeed.raja@gmail.com>
2022-02-04 19:38:54 -0500

    feat:About us link is added

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,10 +1,6 @@
 class Admin::DashboardController < ApplicationController
   def show
     @product = Product.count
-    @categories = Category.count
-    
+    @categories = Category.count    
   end
 end
-
-# Display a count of how many products are in the database
-# Display a count of how many categories are in the database
